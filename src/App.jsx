6a02ce0a749374c6ed1f6e54dca60ba8776f541b
Marcus Kwan <kwan.marcus@gmail.com>
2019-07-10 06:45:15 +1000

    wrote code to render the data from file

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
-import Logo from "./components/DisplayComponents/Logo";
+import Logo from "./components/DisplayComponents/Logo.jsx";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
--- src/components/ButtonComponents/OperatorButtons/Operators.jsx
+++ src/components/ButtonComponents/OperatorButtons/Operators.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
-
+import { OperatorButton } from "./OperatorButton.jsx";
 //Import your array data to from the provided data file
-
-const Specials = () => {
+import { operators } from "../path/to/data";
+export const Operators = () => {
   // STEP 2 - add the imported data to state
-
+  const [operatorsData, setOperator] = useState(operators);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {operatorData.map(operator => (
+        <OperatorButton operator={operator}/>
+      ))}
     </div>
   );
 };
